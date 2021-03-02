db5d433f58fd536432879f645c25961dd20484e9
Saurabh Suresh Powar <66636289+Spnetic-5@users.noreply.github.com>
2021-03-03 01:28:51 +0530

    Initial commit

--- Flask_Blog/flask_blog.py
+++ Flask_Blog/flask_blog.py
@@ -3,4 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1> Hello, World! </h1>'
+    return '<h1> Hello, World B! </h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
