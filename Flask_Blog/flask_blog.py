cb3cb5e3c034e76af7f36b93227274959c6db6b9
Saurabh Suresh Powar <66636289+Spnetic-5@users.noreply.github.com>
2021-03-03 15:36:11 +0530

    added

--- Flask_Blog/flask_blog.py
+++ Flask_Blog/flask_blog.py
@@ -11,7 +11,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return '<h1> About page! </h1>'
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
