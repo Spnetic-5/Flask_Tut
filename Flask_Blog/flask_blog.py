7c403f4a3296a334b48ead94cdd5996da07d384b
Saurabh Suresh Powar <66636289+Spnetic-5@users.noreply.github.com>
2021-03-03 15:39:10 +0530

    Update flask_blog.py

--- Flask_Blog/flask_blog.py
+++ Flask_Blog/flask_blog.py
@@ -1,13 +1,28 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Saurabh Powar',
+        'title' : 'asjd'
+        'content' : 'asdcsvc'
+        'lov' : 'sp'
+    },
+    {
+        'author': 'Sauraxd scbh Powar',
+        'title' : 'sjddcdsac'
+        'content' : 'scxsaxa'
+        'lov' : 'psdsc'
+    }
+]
+
 @app.route('/')
 def root():
     return '<h1> Heyy! </h1>'
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts = posts)
 
 @app.route('/about')
 def about():
